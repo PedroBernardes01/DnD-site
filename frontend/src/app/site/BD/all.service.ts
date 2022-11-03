afa64f489079beefd49cf34612ee57be09bc2e39
PedroBernardes01 <pedrokc55@outlook.com>
2022-11-03 19:34:21 -0300

    .

---
+++ frontend/src/app/site/BD/all.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { user } from './user.model';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AllService {
+
+  baseUrluser = 'http://localhost:3000/user'
+
+  constructor(private http: HttpClient) { }
+
+  postUser(User: user): Observable<user> {
+    return this.http.post<user>(this.baseUrluser, User)
+  }
+}
--- frontend/src/app/site/cadastro/cadastro.component.ts
+++ frontend/src/app/site/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { user } from './../BD/user.model';
+import { AllService } from './../BD/all.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,13 +13,37 @@ export class CadastroComponent implements OnInit {
   
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   senhaFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  senV = 'password'
+  eye = 'visibility_off'
 
-  constructor(private router: Router) { }
+  user: user = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  constructor(private router: Router, private service: AllService) { }
 
   ngOnInit(): void {
   }
 
   goto(rout: string): void {
     this.router.navigate([rout])
+  }
+
+  vis(): void {
+    switch(this.senV){
+      case 'password':
+        this.senV = 'text'
+        this.eye = 'visibility'
+        break;
+      case 'text':
+        this.senV = 'password'
+        this.eye = 'visibility_off'
+        break;
+    }
+  }
+
+  cadas(): void {
   }
 }
